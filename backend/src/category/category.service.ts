9d01b24b48988470f6a01c04e308c77c3d06a511
OmerM86 <omerm@99x.io>
2024-10-15 11:17:00 +0530

    feat(category): default category seeder

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { Category } from './entity/category.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryService {
+export class CategoryService implements OnApplicationBootstrap {
   constructor(
     @InjectRepository(Category)
-    private readonly expenseRepository: Repository<Category>,
+    private readonly categoryRepository: Repository<Category>,
   ) {}
 
+  onApplicationBootstrap(): void {
+    const categories = [
+      {
+        cid: 1,
+        name: 'Food',
+        color: '4ade80'
+      },
+      {
+        cid: 2,
+        name: 'Transport',
+        color: 'A1FE4A'
+      },
+      {
+        cid: 3,
+        name: 'Entertainment',
+        color: '0495D3'
+      },
+    ];
+    categories.forEach(async (category) => {
+      const isExist = await this.categoryRepository.findOne({where : {cid: category.cid}});
+      if (!isExist) {
+        const newCategory = this.categoryRepository.create({name: category.name, color: category.color})
+        this.categoryRepository.insert(newCategory);
+      }
+    })
+  }
+
   async findOne(cid: number): Promise<Category> {
-    return await this.expenseRepository.findOne({ where: { cid: cid } });
+    return await this.categoryRepository.findOne({ where: { cid: cid } });
   }
 
   async findAll(): Promise<Category[]> {
-    return await this.expenseRepository.find();
+    return await this.categoryRepository.find();
   }
 }
